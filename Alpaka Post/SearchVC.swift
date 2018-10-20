8d234c7247da815c2a0b510837ec2bb5f5c67f8c
Fynn Kiwitt <you@example.com>
2018-10-20 11:37:23 +0200

    added searchVC

---
+++ Alpaka Post/SearchVC.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Alpaka Post
+//
+//  Created by Fynn Kiwitt on 20.10.18.
+//  Copyright © 2018 Fynn Kiwitt. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    @IBOutlet weak var startTextField: UITextField!
+    @IBOutlet weak var destTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func searchBtnPressed(_ sender: UIButton) {
+        //search
+        if !areValuesValid(){
+            let alert = UIAlertController(title: "No City Entered", message: "Please check the cities you have entered agian.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func areValuesValid() -> Bool{
+        if startTextField.text != "" && destTextField.text != ""{
+            return true
+        }
+        return false
+    }
+    
+}
+
